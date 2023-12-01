95c08f0041a72eb5e10b240fb1844425db6a0ebc
Daniil <dan.ustinoff2011@yandex.ru>
2023-12-01 21:28:22 +0300

    test 12

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const { PORT = 3000, URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173, https://xc1st.nomoredomainsmonster.ru'],
+  origin: ['https://localhost:5173, https://xc1st.nomoredomainsmonster.ru'],
 }));
 
 const limiter = rateLimit({
