b4879a048ff8d6f8f4a822fd58f476728cecd9bf
Daniil <dan.ustinoff2011@yandex.ru>
2023-12-02 13:32:00 +0300

    test 13

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -5,8 +5,8 @@ const urlRegex = require('../utils/constants');
 
 router.post('/', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(urlRegex),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
